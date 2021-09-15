29742d5baaf2b3e06f1b20a9a7fa49be62bcf581
abhyankartanvi <abhyankar.tanvi1993@gmail.com>
2021-09-15 20:55:35 +1000

    updates to timer and read me file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # 04-js-quiz
+
+Description: Javascript Quiz project was my homework assignment for Monash University's coding bootcamp.
+
+This project utlises HTML, CSS and Javascript to render a timed quiz.
+
+Quiz is yet to be completed.
+
+Accessiblity: You are able to access this project on GitHub.
+
+Link to the delployed page:
+https://abhyankartanvi.github.io/updated_password_generator/
--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,18 @@
 // variables to be used
-let timeEl = document.querySelector("p.time");
+let timeEl = document.querySelector("#time");
 let timeLeft = 75;
 let questionCount = 0;
 var startBtn = document.querySelector("#start");
 let intro = document.querySelector(".introduction");
 var ques = document.querySelector("#questions");
 let questionEl = document.querySelector("#question");
+let optionbtn = document.querySelectorAll(".answer-button");
 let option1 = document.querySelector("#answer1");
 let option2 = document.querySelector("#answer2");
 let option3 = document.querySelector("#answer3");
 let option4 = document.querySelector("#answer4");
+let result = document.querySelector("#result");
+var timerInterval;
 
 // list of questions - create an array of objects
 var questionsList = [
@@ -17,7 +20,7 @@ var questionsList = [
     // question 1
     question: "Commonly used data types do NOT include",
     options: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
-    correctOption: "3",
+    correctOption: "2",
   },
   // question 2
   {
@@ -29,7 +32,7 @@ var questionsList = [
       "3. parentheses",
       "4. square brackets",
     ],
-    correctOption: "3",
+    correctOption: "1",
   },
   // question 3
   {
@@ -40,34 +43,32 @@ var questionsList = [
       "3. Booleans",
       "4. All of the above",
     ],
-    correctOption: "4",
+    correctOption: "3",
   },
   // question 4
   {
     question:
       "String values must be enclosed within ____ when being assigned to variables.",
     options: ["1. Commas", "2. Curly Brackets", "3. Quotes", "4. Parantheses"],
-    correctOption: "4",
+    correctOption: "2",
   },
   // question 5
   {
     question:
       "A very useful tool used during development and debugging for printing content to the debugger is",
     options: ["1. Javascript", "2. Terminal", "3. for loops", "4.console.log"],
-    correctOption: "4",
+    correctOption: "3",
   },
 ];
 
 // set timer interval and invoke it when start is clicked. Timer to reduce and stop if the seconds left is 0 or total questions have been exhausted
 function timerDisplay() {
-  let timerInterval = setInterval(function () {
-    timeLeft--;
-    timeEl.textContent = timeLeft;
+  timeLeft--;
+  timeEl.textContent = timeLeft + " seconds left";
 
-    if (timeLeft === 0 || questionCount === questionsList.length) {
-      clearInterval(timerInterval);
-    }
-  }, 1000);
+  if (timeLeft === 0 || questionCount === questionsList.length) {
+    clearInterval(timerInterval);
+  }
 }
 
 //function to be invoked when start button is clicked. Change display to display the questions and hide the introduction content
@@ -77,7 +78,7 @@ function startQuiz() {
   ques.style.display = "block";
   questionCount = 0;
 
-  timerDisplay();
+  timerInterval = setInterval(timerDisplay, 1000);
   displayQuestion(questionCount);
 }
 
@@ -93,3 +94,39 @@ function displayQuestion(id) {
 
 // event listener for start button
 startBtn.addEventListener("click", startQuiz);
+
+function nextQuestion() {
+  if (questionCount < questionsList.length) {
+    questionCount++;
+  }
+  displayQuestion(questionCount);
+}
+
+// function to show correct or wrong
+function displayResult(event) {
+  event.preventDefault();
+  result.style.display = "block";
+  let div = document.createElement("div");
+  result.appendChild(div);
+
+  setTimeout(function () {
+    div.style.display = "none";
+  }, 1000);
+
+  if (questionsList[questionCount].correctOption === event.target.value) {
+    div.textContent = "Correct!";
+  } else if (
+    questionsList[questionCount].correctOption !== event.target.value
+  ) {
+    timeLeft = timeLeft - 10;
+    div.textContent = "Wrong!";
+  }
+}
+
+optionbtn.forEach((element) => {
+  element.addEventListener("click", nextQuestion);
+});
+
+optionbtn.forEach((element) => {
+  element.addEventListener("click", displayResult);
+});
